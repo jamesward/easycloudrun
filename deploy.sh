fabd0a1e62007aff1f58e3003ce9caba81a4451b
James Ward <james@jamesward.com>
2021-03-23 17:02:44 -0600

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+
+#set -uxo pipefail
+
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+
+if [[ -z "${PROJECT_ID}" ]]; then
+  echo "PROJECT_ID env var not set"
+  exit 1
+fi
+
+if [[ -z "${IMAGE_NAME}" ]]; then
+  echo "IMAGE_NAME env var not set"
+  exit 1
+fi
+
+if [[ -z "${REGION}" ]]; then
+  echo "REGION env var not set"
+  exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. $DIR/libs/build_id_to_trigger_id.sh
+
+$DIR/libs/deploy.sh
