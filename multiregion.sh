78c9ed372bae5f0bc23925c105bc9c70d55f76ec
James Ward <james@jamesward.com>
2021-05-04 08:18:42 -0600

    multiregion DEPLOY_OPTS

--- multiregion.sh
+++ multiregion.sh
@@ -131,7 +131,7 @@ readonly REGIONS=$(gcloud run regions list --project $PROJECT_ID --format="value
 
 #readonly REGIONS="us-central1 us-west1"
 
-export DEPLOY_OPTS="--ingress=internal-and-cloud-load-balancing"
+export DEPLOY_OPTS="--ingress=internal-and-cloud-load-balancing $DEPLOY_OPTS"
 
 for REGION in $REGIONS; do
   export REGION
